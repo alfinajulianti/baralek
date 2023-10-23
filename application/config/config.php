36b3cc1428c95d2086e8e9634802bc024980ee9d
alfinajulianti <221910918@stis.ac.id>
2023-10-24 04:40:17 +0700

    ganti lgi base url jdi localhost

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'baralek-bpskotasungaipenuh.vercel.app';
+$config['base_url'] = 'http://localhost/baralek/';
 
 /*
 |--------------------------------------------------------------------------
