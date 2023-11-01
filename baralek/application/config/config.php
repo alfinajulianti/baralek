12c4e6940e27e95475fce461f29362285a0a99bd
alfinajulianti <221910918@stis.ac.id>
2023-11-01 12:01:20 +0700

    menambahkan layanan

--- baralek/application/config/config.php
+++ baralek/application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://bpssungaipenuh.com/baralek/';
+//$config['base_url'] = 'http://localhost/baralek/baralek/';
 
 /*
 |--------------------------------------------------------------------------
--- baralek/application/views/beranda.php
+++ baralek/application/views/beranda.php
@@ -28,5 +28,59 @@
 </section><!-- End Hero -->
 
 <main id="main">
+    <section id="values" class="values">
+        <div class="container aos-init aos-animate" data-aos="fade-up">
+
+            <header class="section-title-header">
+                <h2 data-aos="fade-up">Layanan BPS</h2>
+            </header>
+
+            <div class="row d-flex justify-content-center">
+                <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="200">
+                    <a href="http://s.bps.go.id/maisinggahbpssupe" target="_blank">
+                        <div class="box d-flex flex-column justify-content-between">
+                            <img src="<?= base_url() ?>assets/img/SDI/mai-singgah.png" class="img-fluid logo-riset" alt="">
+                            <h3>MAI SINGGAH <br> <small>(Virtual Office BPS Kota Sungai Penuh)</small></h3>
+                            <h6>Pelayanan data, konsultasi statistik, permintaan publikasi, saran dan kritik, pengaduan dan pelayanan sejenisnya di BPS Kota Sungai Penuh secara virtual melalui gadget/laptop.</h6>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="400">
+                    <a href="https://sirusa.web.bps.go.id/" target="_blank">
+                        <div class="box d-flex flex-column justify-content-between">
+                            <img src="<?= base_url() ?>assets/img/SDI/sirusa.png" class="img-fluid logo-riset" alt="">
+                            <h3>SIRuSa <br> <small>(Sistem Informasi Rujukan Statistik)</small></h3>
+                            <h6>Website yang menyediakan informasi metadata kegiatan statistik yang ada di Indonesia. Dengan adanya SiRusa, diharapkan duplikasi kegiatan statistik dapat dihindari.</h6>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-6" data-aos="fade-up" data-aos-delay="600">
+                    <a href="https://romantik.web.bps.go.id/" target="_blank">
+                        <div class="box d-flex flex-column justify-content-between">
+                            <img src="<?= base_url() ?>assets/img/SDI/logo-romantik.svg" class="img-fluid logo-riset" alt="">
+                            <h3>ROMANTIK <br> <small>(Rekomendasi Kegiatan Statistik)</small></h3>
+                            <h6>Layanan bagi instansi pemerintah untuk mendapatkan rekomendasi penyelenggaraan kegiatan statistik sektoral dari BPS.</h6>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+                    <a href="https://indah.bps.go.id/" target="_blank">
+                        <div class="box d-flex flex-column justify-content-between">
+                            <img src="<?= base_url() ?>assets/img/SDI/indah.svg" class="img-fluid logo-riset" alt="">
+                            <h3>INDAH <br> <small>(Indonesia Data Hub)</small></h3>
+                            <h6></h6>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <br>
+            <div class="row d-flex justify-content-center">
+            </div>
+        </div>
+    </section>
+
 
 </main><!-- End #main -->
